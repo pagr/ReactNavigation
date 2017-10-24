1d9ca67d410896a0e78bc841995cd27a2242c65c
Paul Griffin <paul.griffin@hotmail.com>
2017-10-24 20:13:03 +0200

    Firsty

---
+++ App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { StyleSheet, Text, View, TabBarIOS, NavigatorIOS, Button } from 'react-native';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {selectedTab: 'tabFavorites'};
+  }
+  setTab(tabId) {
+    this.setState({selectedTab: tabId});
+  }
+  render() {
+    return (
+      <TabBarIOS>
+        <TabBarIOS.Item 
+          systemIcon="favorites"
+          selected={this.state.selectedTab === 'tabFavorites'}
+          onPress={() => this.setTab('tabFavorites')}>
+          <NavigatorIOS
+            initialRoute={{
+              component: MyScene,
+              title: 'My Initial Scene',
+            }}
+            style={{flex: 1}}
+          />
+        </TabBarIOS.Item>
+        <TabBarIOS.Item 
+          systemIcon="downloads"
+          selected={this.state.selectedTab === 'tabDownloads'}
+          onPress={() => this.setTab('tabDownloads')}>
+          <View>
+            <Text>Hello second tab</Text>
+          </View>
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    );
+  }
+}
+
+class MyScene extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {color: '#ff0000'};
+  }
+  navigateToNext() {
+    this.setState({color:'#abcdef'})
+  }
+  render() {
+    return (
+      <View style={styles.container} backgroundColor={this.state.color}>
+        <Button title={"Click here"} onPress={() => this.navigateToNext}/>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
